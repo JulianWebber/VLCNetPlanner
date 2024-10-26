1fe746b376991475b4661f3fe143e35c28be7d58
jwebber3 <30943676-jwebber3@users.noreply.replit.com>
2024-10-26 18:29:13 +0000

    Agent query: Can you verify if: 1. The floor level controls are working? 2. Components can be added to different floors? 3. The export configuration button works without errors?

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -1,19 +1,24 @@
 import json
 import numpy as np
 
-def export_configuration(floor_plan, components, coverage_analysis):
+def export_configuration(floor_plans, components, coverage_analysis):
     """Export VLC network configuration to JSON format"""
     config = {
-        'floor_plan': {
-            'width': floor_plan['width'],
-            'height': floor_plan['height'],
-            'type': floor_plan['type']
+        'floor_plans': {
+            level: {
+                'width': floor_data['width'],
+                'height': floor_data['height'],
+                'type': floor_data['type'],
+                'ceiling_height': floor_data.get('ceiling_height', 3.0)
+            }
+            for level, floor_data in floor_plans.items()
         },
         'components': [
             {
                 'id': comp['id'],
                 'type': comp['type'],
                 'position': comp['position'],
+                'floor_level': comp.get('floor_level', 0),
                 'properties': comp['properties']
             }
             for comp in components
